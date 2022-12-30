c5e6a4aba6e2a22de5691ab8b29d10091dfa7cb2
semVer4 <nikrozhko125@gmail.com>
2022-12-30 23:28:14 +0300

    save

--- weatherApp/App.js
+++ weatherApp/App.js
@@ -1,11 +1,14 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  const response = axios.get(url);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text></Text>
     </View>
   );
 }
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'silver',
     alignItems: 'center',
     justifyContent: 'center',
   },
