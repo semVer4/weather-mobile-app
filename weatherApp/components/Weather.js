c2a69599546f348fac2b8242866f71a2584d62ba
semVer4 <nikrozhko125@gmail.com>
2023-01-02 20:55:30 +0300

    save

--- weatherApp/components/Weather.js
+++ weatherApp/components/Weather.js
@@ -2,49 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Text, StatusBar } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
-
-const weatherOptions = {
-    Thunderstorm: {
-        iconName: 'weather-lightning',
-        gradient: ['#141E30', '#243B55']
-    },
-    Drizzle: {
-        iconName: 'weather-rainy',
-        gradient: ['#3a7bd5', '#3a6073']
-    },
-    Rain: {
-        iconName: 'weather-pouring',
-        gradient: ['#000046','#1CB5E0']
-    },
-    Snow: {
-        iconName: 'snowflake',
-        gradient: ['#83a4d4', '#b6fbff']
-    },
-    Dust: {
-        iconName: 'weather-windy-variant',
-        gradient: ['#B79891', '#94716B']
-    },
-    Smoke: {
-        iconName: 'weather-windy',
-        gradient: ['#56CCF2', '#2F80ED']
-    },
-    Haze: {
-        iconName: 'weather-hazy',
-        gradient: ['#3E5151', '#DECBA4']
-    },
-    Mist: {
-        iconName: 'weather-fog',
-        gradient: ['#606c88', '#3f4c6b']
-    },
-    Clear: {
-        iconName: 'weather-sunny',
-        gradient: ['#56CCF2', '#2F80ED']
-    },
-    Clouds: {
-        iconName: 'weather-cloudy',
-        gradient: ['#757F9A', '#D7DDE8']
-    },
-};
+import weatherOptions from '../weatherOptions';
 
 const Weather = ({temp, city, weather, desc}) => {
     return (
